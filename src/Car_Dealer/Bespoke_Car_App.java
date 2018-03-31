c7a3fb38a7fafee67e041ce05f0ffc5a0d796ad5
Arexss <a.buonerba@hotmail.com>
2018-03-31 18:26:19 +0100

    First Commit

---
+++ src/Car_Dealer/Bespoke_Car_App.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Car_Dealer;
+
+/**
+ *
+ * @author Alessandro Buonerba
+ * 24/03/2018
+ */
+public class Bespoke_Car_App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Create new instance of Login_Window and display it. 
+        Login_Window frm = new Login_Window();
+        frm.setVisible(true);
+    }
+    
+}
